adf3431cd36454cba0a05cc413faf5862ee3e9bb
Aradhya Jain <aradhyajain471@gmail.com>
2024-12-01 22:46:05 +0530

    Update server logging

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,20 +4,20 @@ module.exports = (app, fileStore) => {
       const filename = req.headers['x-filename'];
       const content = req.body;
       const result = fileStore.addFile(filename, content.toString());
-      console.log(`File is Added.`);
+      console.log(`A File is Added.`);
       res.json(result);
     });
   
     // List files
     app.get('/files', (req, res) => {
+      console.log(`Fetching existing files on the server.`);
       res.json(fileStore.listFiles());
-      console.log(`List of the Files:`);
     });
   
     // Remove file
     app.delete('/files/:filename', (req, res) => {
       const result = fileStore.removeFile(req.params.filename);
-      console.log(`Files are Removed.`);
+      console.log(`A File is Removed.`);
       res.json(result);
     });
   
@@ -26,21 +26,21 @@ module.exports = (app, fileStore) => {
       const filename = req.params.filename;
       const content = req.body;
       const result = fileStore.updateFile(filename, content.toString());
-      console.log(`Files are Updated.`);
+      console.log("Updated the file", filename);
       res.json(result);
     });
   
     // Word count
     app.get('/files/wordcount', (req, res) => {
+      console.log(`Calculating Word Count`);
       res.json({ wordCount: fileStore.getWordCount() });
-      console.log(`Word Count`);
     });
   
     // Frequent words
     app.get('/files/frequent-words', (req, res) => {
       const limit = parseInt(req.query.limit) || 10;
       const order = req.query.order || 'dsc';
+      console.log(`Fetching Frequent Words`);
       res.json(fileStore.getFrequentWords(limit, order));
-      console.log(`Frequent Words :`);
     });
   };
